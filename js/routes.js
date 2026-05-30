b0232b8b403f5babb177d94232178c4c97a15b37
dwunco <declanlin214@gmail.com>
2026-05-31 00:45:42 +0800

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -1,9 +1,11 @@
+import Welcome from './pages/Welcome.js'; // Imports your new homepage view
 import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 
 export default [
-    { path: '/', component: List },
+    { path: '/', component: Welcome }, // Welcome tab is now the default landing page
+    { path: '/list', component: List },   // The main Demonlist moves to this path
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
 ];
