96e726f0edbdde6697ef13d42796a32656705133
Declan Lin <declanlin214@gmail.com>
2026-07-22 22:22:42 +0800

    twenty trials placed at #15

--- fix.py
+++
@@ -1,3 +0,0 @@
-string = """node add-level.mjs “72" "platformer" 3 112603907 “72” “Polarbeahr” "Polarbeahr" “cookiedarookie” "https://youtu.be/LiT1SygzBXc" 100 “Free Copy” "" “silver”"""
-newstring = string.replace("“", '"').replace("”", '"')
-print(newstring)
