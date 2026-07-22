5ccaf7b0855d2c915322e5cb14e0ec84e3056aa4
Declan Lin <declanlin214@gmail.com>
2026-07-22 17:53:29 +0800

    boxing boxes placed at #106

--- fix.py
+++ fix.py
@@ -1,3 +1,3 @@
-string = """node add-level.mjs “Lonely travel” "classic" 83 17924880 “Lonely_travel” “FunnyGame” "FunnyGame" “fractal” "https://youtu.be/null" 100 "248349" “2.0, XXL+, Nerve Control, Slow-Paced, Chokepoints, UFO” “bronze”"""
+string = """node add-level.mjs “Boxing Boxes” "classic" 106 94890873 “Boxing_Boxes” “Ellisha” "Ellisha" “fractal” "https://youtu.be/ilWAkVEghLY" 100 “Free “Copy “2.1, Long, Learny, Gimmicky, Memory,” “bronze”"""
 newstring = string.replace("“", '"').replace("”", '"')
 print(newstring)
